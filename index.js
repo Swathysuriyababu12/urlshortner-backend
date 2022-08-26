025773d9d5b49c38da1e1071168ed5a2041dfa15
Swathysuriyababu12 <swathysuryababu@gmail.com>
2022-08-26 14:10:21 +0530

    change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.get("/", async (req, res) => {
   const shortUrls = await url.find();
-  //res.send({ short: shortUrls });
+  res.send("All is well and listening");
 });
 
 app.get("/:shortUrl", async (req, res) => {
